6353d40ea743e249390078cbcec2c6eb9825fcc5
Nico <nico.zimmermann@it.htl-hl.ac.at>
2024-11-08 09:53:15 +0100

    holzi commit

---
+++ vinoventure_web/frontend/src/pages/Shop.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/MainComponents/Footer";
+import Navbar from "../components/MainComponents/Navbar";
+
+function Shop() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default Shop;
